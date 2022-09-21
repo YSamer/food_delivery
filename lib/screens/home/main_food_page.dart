e80707d8daafd121a7f82fd102270f6acbc76869
YSamer <ysamer2525@gmail.com>
2022-09-21 12:38:41 +0200

    first commit

---
+++ lib/screens/home/main_food_page.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class MainFoodPage extends StatefulWidget {
+  const MainFoodPage({Key? key}) : super(key: key);
+
+  @override
+  State<MainFoodPage> createState() => _MainFoodPageState();
+}
+
+class _MainFoodPageState extends State<MainFoodPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Container(
+          child: Row(
+            children: [
+              Column(
+                children: [
+                  Text('Country'),
+                  Text('City'),
+                ],
+              ),
+              Container(
+                height: 45,
+                width: 45,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  color: Colors.blue,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
